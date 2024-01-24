afab67d16338dc1b0e1c5db685cbb3edae351bf7
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-24 16:42:43 +0300

    Used Keyboarde Aware Scroll View in the logn screen

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -1,6 +1,7 @@
 //import liraries
 import React, { Component, useState } from 'react';
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const backImage = require("../../assets/background_signin.jpg")
 
@@ -13,12 +14,12 @@ const LoginScreen = () => {
 
     console.log(values)
     return (
-        <>
+        <KeyboardAwareScrollView>
             <View>
                 <Image source={backImage} className={`object-cover h-80 w-full`}/>
             </View>
-            <View className="bg-white h-screen rounded-t-3xl">
-                <Text className="text-[#d60e45] text-3xl font-semibold text-center py-3 mt3">
+            <View className="bg-white h-screen rounded-t-3xl mt-4">
+                <Text className="text-[#d60e45] text-3xl font-semibold text-center py-3 mt-3">
                     Sign in{" "}
                 </Text>
                 <View className='items-center w-full justify-center'>
@@ -46,7 +47,7 @@ const LoginScreen = () => {
                     </Text>
                 </TouchableOpacity>
             </View>
-        </>
+        </KeyboardAwareScrollView>
     );
 };
 
