adb498c5621f9f10a43f24d2b4e2860c4f1d10eb
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-01-25 14:06:57 +0300

    Designed the Sign Up page

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import LoginScreen from './src/Screens/LoginScreen';
+import SingupScreen from './src/Screens/SingupScreen';
 
 export default function App() {
   return (
     <View>
-      <LoginScreen />
+      {/* <LoginScreen /> */}
+      <SingupScreen />
     </View>
   );
 }
